d81ac9292c9af76e75766361496690c7d4e22921
vanvac <167641733+vanvac@users.noreply.github.com>
2025-03-06 13:53:03 +0000

    small changes im working on it and procastinating

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -2,12 +2,10 @@
 #include <cstdint>
 #include <vector>
 
-class Position{
+class BitBoard{
   std::vector<int64_t>  whitePawnBitBoard, whiteRookBitBoard, whiteBishopBitBoard, whiteQueenBitBoard, whiteKingBitBoard,
                         blackPawnBitBoard, blackRookBitBoard, blackBishopBitBoard, blackQueenBitBoard, blackKingBitBoard;
-  void serializeBitBoardToFen(){
-      
-  }
-  void visualisePosition(){}
-
 };
+
+
+
